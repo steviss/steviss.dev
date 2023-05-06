7af4a0cf69a77d14295a3b02e5dab874df000394
Stefan Stevic <steviss@gmail.com>
2023-05-06 20:17:27 +0200

    chore(setup): project setup

---
+++ eslintrc.cjs
@@ -0,0 +1,73 @@
+module.exports = {
+  plugins: [
+    "react",
+    "react-redux",
+    "@typescript-eslint",
+    "react-hooks",
+    "prettier",
+    "no-relative-import-paths",
+    "testing-library",
+    "plugin:astro/recommended",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:testing-library/react",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-redux/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "no-console": 1,
+    "prettier/prettier": 2,
+    "testing-library/await-async-query": "error",
+    "testing-library/no-await-sync-query": "error",
+    "testing-library/no-debugging-utils": "warn",
+    "testing-library/no-dom-import": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "req|res|next|val" },
+    ],
+    "no-relative-import-paths/no-relative-import-paths": [
+      "error",
+      { allowSameFolder: true },
+    ],
+  },
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  root: true,
+};
---
+++ prettierrc.cjs
@@ -0,0 +1,31 @@
+module.export = {
+  printWidth: 140,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  importOrder: [
+    "^src/types|src/utilities(.*)$",
+    "^src/components/store(.*)$",
+    "^src/pages(.*)$",
+    "^src/components/organisms(.*)$",
+    "^src/components/molecules(.*)$",
+    "^src/components/atoms(.*)$",
+    "^src/store(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
