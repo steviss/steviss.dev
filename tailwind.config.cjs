37b2dee5edaddb59732865a150c4fcc63f7dee74
steviss <steviss@gmail.com>
2023-08-13 15:38:46 +0200

    chore(project-setup): reshuffle the design a bit, add glassmorhpism

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        glass: "0 0.25rem 2rem 0 rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+  plugins: [],
+};
