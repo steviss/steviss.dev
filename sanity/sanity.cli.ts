291cfac2c994f89ba9c382199ee17c8e4c2a31f4
steviss <steviss@gmail.com>
2023-08-22 19:28:04 +0200

    chore(docker-compose): making the setup a bit cleaner, created a Dockerfile and dockercompose.yml to run Sanity Studio

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -5,4 +5,8 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
+  server: {
+    hostname: '0.0.0.0', //so the docker instance routes properly
+    port: 3333,
+  },
 })
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,8 +1,8 @@
+import {env} from './env.mjs'
+import {schemaTypes} from './schemas'
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
-import {env} from './env.mjs'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +10,6 @@ export default defineConfig({
   projectId: env.SANITY_STUDIO_PROJECT_ID,
   dataset: env.SANITY_STUDIO_DATASET,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
