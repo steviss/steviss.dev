fed5b45548e8548b25415e3d4cf3f731b665749d
Stefan Stevic <steviss@gmail.com>
2023-09-05 05:17:22 +0200

    setup(typescript): improve the code, setup absolute paths

---
+++ src/types/PostType.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  title: string;
+  slug: {
+    current: string;
+  };
+  description: string;
+  pubDate: string;
+  updatedDate: string;
+  heroImage: any;
+  body: any;
+};
---
+++ src/utility/convertToPostType.ts
@@ -0,0 +1,8 @@
+import type { PostType } from "src/types";
+
+export const convertToPostType = (posts: any[]): PostType[] =>
+  posts.map(({ title, slug, _createdAt }) => ({
+    title,
+    slug: slug.current,
+    pubDate: _createdAt,
+  })) as PostType[];
