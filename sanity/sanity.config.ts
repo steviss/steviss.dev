c497169640b8d7fb3129f400fcb1d9c9d008344e
Stefan Stevic <steviss@gmail.com>
2023-09-07 13:56:25 +0200

    chore(sanity): fix some things regarding sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,4 +1,3 @@
-import { env } from './env.mjs'
 import { schemaTypes } from './schemas'
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
@@ -7,8 +6,8 @@ import { deskTool } from 'sanity/desk'
 export default defineConfig({
   name: 'default',
   title: 'steviss-dev-sanity',
-  projectId: env.SANITY_STUDIO_PROJECT_ID,
-  dataset: env.SANITY_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? '',
+  dataset: process.env.SANITY_STUDIO_DATASET ?? '',
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
