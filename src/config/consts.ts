8b7c9c44d4421eea54ce0b347bb7db46153f2247
Stefan Stevic <steviss@gmail.com>
2023-09-07 10:23:23 +0200

    chore(dry): fix the code to be a bit more DRY'

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -4,3 +4,20 @@
 export const SITE_TITLE = 'Stefan Stević - steviss - Team Lead & Full-stack Developer'
 export const SITE_DESCRIPTION = 'Team Lead & Full-stack Developer'
 export const SITE_OWNER = 'Stefan Stević'
+export const SITE_TECHNOLOGIES = [
+  'Astro',
+  'Figma',
+  'React',
+  'Next.js',
+  'Node.js',
+  'TypeORM',
+  'Nest.js',
+  'Redux Toolkit',
+  'Apollo Federation',
+  'GraphQL',
+  'REST',
+  'Kafka',
+  'PostgreSQL',
+  'GitHub Actions',
+  'Docker',
+]
