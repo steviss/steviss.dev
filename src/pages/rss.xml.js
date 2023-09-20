682ef03655e2fff66c1f0b4e70eef4ab395fbfb5
steviss <steviss@gmail.com>
2023-09-20 08:28:10 +0200

    ci(rss): fix rss feed warnings

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
 import rss from '@astrojs/rss'
-import { getCollection } from 'astro:content'
+import { useSanityClient } from '@sanity/astro'
+import { convertSanityPostToPost, sanityPostsGroqQuery } from 'src/clients/sanity'
 import { SITE_DESCRIPTION, SITE_TITLE } from 'src/config'
 
-export async function get(context) {
-  const posts = await getCollection('blog')
+export async function GET(context) {
+  const posts = await useSanityClient().fetch(sanityPostsGroqQuery)
+  const postsTyped = convertSanityPostToPost(posts)
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
+    items: postsTyped.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: post.slug,
     })),
   })
 }
